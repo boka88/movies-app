95e51cfbe7b649468e3f1592ebb344a8a509b67d
Boka88 <boka8boka@gmail.com>
2022-07-05 18:22:52 +0200

    add react bootstrap import and Navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
 
 
 function App() {
 
   return (
     <>
+        <Navbar bg="light" expand="lg" variant="dark"><h3 style={{color:'blue',margin:'0 auto'}}>Trending Movies</h3></Navbar>
     </>
-   
   );
 }
 
