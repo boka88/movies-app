b9a19cf9a1dd0eea52c88758f25a7ddb9b8634df
Boka88 <boka8boka@gmail.com>
2022-07-05 18:26:08 +0200

    add initial api call and needed hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from 'react-bootstrap';
 
+const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=7ee58e2a9e9084ce0102afe70a68945c";
+
 
 function App() {
+
+  const [movies, setMovies]=useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then((res)=>res.json())
+    .then(data=>{
+      console.log(data);
+      setMovies(data.results);
+    })
+  }, [])
+
+
 
   return (
     <>
